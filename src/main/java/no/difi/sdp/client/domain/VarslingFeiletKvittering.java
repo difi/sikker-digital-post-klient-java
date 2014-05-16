2cbfafef1dbe3f1a5bdd3ffda1d76fb920dc8ab9
Hong Nhung Thi Vo <hong.nhung.thi.vo@bekk.no>
2014-05-16 12:42:00 +0200

    Added tidspunkt to VarslingFeiletKvittering

--- src/main/java/no/difi/sdp/client/domain/VarslingFeiletKvittering.java
+++ src/main/java/no/difi/sdp/client/domain/VarslingFeiletKvittering.java
@@ -1,8 +1,11 @@
 package no.difi.sdp.client.domain;
 
+import java.util.Date;
+
 public class VarslingFeiletKvittering {
 
     private Varslingskanal varslingFeiletKanal;
+    private Date tidspunkt;
     private String feilbeskrivelse;
 
 }
