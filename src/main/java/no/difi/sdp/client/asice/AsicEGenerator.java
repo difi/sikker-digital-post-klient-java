958b9f8e2751aad2ece2516089243fbd7cdafd12
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-05-22 10:13:03 +0200

    Create support for writing sent asic-e files to disk for debug

--- src/main/java/no/difi/sdp/client/asice/AsicEGenerator.java
+++ src/main/java/no/difi/sdp/client/asice/AsicEGenerator.java
@@ -3,8 +3,14 @@ package no.difi.sdp.client.asice;
 import no.difi.sdp.client.asice.signature.CreateSignature;
 import no.difi.sdp.client.domain.Avsender;
 import no.difi.sdp.client.domain.Forsendelse;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +20,10 @@ public class AsicEGenerator {
     private final CreateManifest createManifest;
     private final CreateSignature createSignature;
     private final CreateZip createZip;
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private static File debug_writeToDisk = null;
 
     public AsicEGenerator() {
         createManifest = new CreateManifest();
@@ -37,7 +47,45 @@ public class AsicEGenerator {
         // Zip filene
         Archive archive = createZip.zipIt(files);
 
+        if (debug_writeToDisk != null) {
+            writeArchiveToDisk(archive);
+        }
+
         return new ByteArrayInputStream(archive.getBytes());
     }
 
+
+    private void writeArchiveToDisk(Archive archive) {
+        log.error("Writing Asic-E to disk for debug");
+        File file;
+        if (debug_writeToDisk.isDirectory()) {
+            file = new File(debug_writeToDisk, "asic-" + System.currentTimeMillis() + ".zip");
+        }
+        else {
+            file = debug_writeToDisk;
+        }
+
+        FileOutputStream output = null;
+        try {
+            output = new FileOutputStream(file);
+            IOUtils.copy(new ByteArrayInputStream(archive.getBytes()), output);
+            log.info("Wrote Asic-E debug file to " + file.getAbsolutePath());
+        } catch (IOException e) {
+            log.error("Could not write Asic-E file", e);
+        } finally {
+            IOUtils.closeQuietly(output);
+        }
+    }
+
+    /**
+     * Skriv zippet Asic-E til disk for manuell inspeksjon.
+     *
+     * @deprecated Ikke for produksjonsbruk.
+     *
+     * @param location Mappe eller fil asic-filen skal skrives til. Hvis det er en mappe vil filnavn bli generert automatisk.
+     */
+    @Deprecated
+    public static void debug_writeArchiveToDisk(File location) {
+        debug_writeToDisk = location;
+    }
 }
