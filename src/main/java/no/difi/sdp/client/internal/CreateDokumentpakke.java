db441e7f6be50161644ebb2c8874476e8553b072
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-05-26 09:36:41 +0200

    Close stream (althought it in practice will be a bytearrayinputstream). Better safe than sorry.

--- src/main/java/no/difi/sdp/client/internal/CreateDokumentpakke.java
+++ src/main/java/no/difi/sdp/client/internal/CreateDokumentpakke.java
@@ -4,11 +4,9 @@ import no.difi.sdp.client.asice.CreateASiCE;
 import no.difi.sdp.client.domain.Avsender;
 import no.difi.sdp.client.domain.Forsendelse;
 import no.difi.sdp.client.domain.Sertifikat;
-import no.difi.sdp.client.domain.exceptions.RuntimeIOException;
-import org.apache.commons.io.IOUtils;
+import no.difi.sdp.client.util.IOUtils;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class CreateDokumentpakke {
@@ -24,17 +22,14 @@ public class CreateDokumentpakke {
     public InputStream createDokumentpakke(Avsender avsender, Forsendelse forsendelse) {
         InputStream asicStream = createASiCE.createStream(avsender, forsendelse);
 
-        byte[] bytes;
-        try {
-            bytes = IOUtils.toByteArray(asicStream);
-        } catch (IOException e) {
-            throw new RuntimeIOException(e);
-        }
+        byte[] bytes = IOUtils.toByteArrayCloseStream(asicStream);
 
         Sertifikat mottakerSertifikat = forsendelse.getDigitalPost().getMottaker().getSertifikat();
 
         CMSDocument cms = createCMS.createCMS(bytes, mottakerSertifikat);
         return new ByteArrayInputStream(cms.getBytes());
     }
+
+
 
 }
