fddf4fcb5711814e6b403e56824593cd3f84cc5c
Hong Nhung Thi Vo <hong.nhung.thi.vo@bekk.no>
2014-05-26 15:08:52 +0200

    WIP: Extended BekreftelsesKvittering, will probably change as we are going to merge hentKvittering and bekreftKvittering

--- src/main/java/no/difi/sdp/client/domain/kvittering/BekreftelsesKvittering.java
+++ src/main/java/no/difi/sdp/client/domain/kvittering/BekreftelsesKvittering.java
@@ -5,5 +5,27 @@ public class BekreftelsesKvittering {
     private String konversasjonsId;
     private String messsageId;
 
+    private BekreftelsesKvittering(String konversasjonsId, String messsageId) {
+        this.konversasjonsId = konversasjonsId;
+        this.messsageId = messsageId;
+    }
 
+    public static Builder builder(String konversasjonsId, String messageId) {
+        return new Builder(konversasjonsId, messageId);
+    }
+
+    public static class Builder {
+        private BekreftelsesKvittering target;
+        private boolean built = false;
+
+        public Builder(String konversasjonsId, String messageId) {
+            target = new BekreftelsesKvittering(konversasjonsId, messageId);
+        }
+
+        public BekreftelsesKvittering build() {
+            if (built) throw new IllegalStateException("Can't build twice");
+            built = true;
+            return target;
+        }
+    }
 }
