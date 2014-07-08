0a6aa054a01567a260bd5d90f894c5a696a6cdd7
Stefan Magnus Landrø <stefan.landro@gmail.com>
2014-07-08 07:36:55 +0200

    updated to reflect changes in sdp-shared 1.1-SNAPSHOT

--- src/main/java/no/difi/sdp/client/ExceptionMapper.java
+++ src/main/java/no/difi/sdp/client/ExceptionMapper.java
@@ -19,9 +19,11 @@ import no.difi.sdp.client.domain.exceptions.EbmsException;
 import no.difi.sdp.client.domain.exceptions.SendException;
 import no.difi.sdp.client.domain.exceptions.SendIOException;
 import no.difi.sdp.client.domain.exceptions.SoapFaultException;
-import no.digipost.api.EbmsClientException;
-import org.springframework.ws.client.WebServiceIOException;
-import org.springframework.ws.soap.client.SoapFaultClientException;
+import no.difi.sdp.client.domain.exceptions.ValideringException;
+import no.digipost.api.exceptions.MessageSenderEbmsErrorException;
+import no.digipost.api.exceptions.MessageSenderIOException;
+import no.digipost.api.exceptions.MessageSenderSoapFaultException;
+import no.digipost.api.exceptions.MessageSenderValidationException;
 
 /**
  * Exception mapper for sending av sikker digital post. Gjør subclassing av denne for implementere egen/tilpasset feilhåndtering.
@@ -39,15 +41,14 @@ public class ExceptionMapper {
         // Don't attempt to map SendExceptions, they are already correct type
         if (e instanceof SendException) {
             return (SendException) e;
-        }
-        else if (e instanceof EbmsClientException) {
-            return new EbmsException((EbmsClientException) e);
-        }
-        else if (e instanceof WebServiceIOException) {
+        } else if (e instanceof MessageSenderEbmsErrorException) {
+            return new EbmsException((MessageSenderEbmsErrorException) e);
+        } else if (e instanceof MessageSenderIOException) {
             return new SendIOException(e);
-        }
-        else if (e instanceof SoapFaultClientException) {
-            return new SoapFaultException((SoapFaultClientException) e);
+        } else if (e instanceof MessageSenderValidationException) {
+            return new ValideringException((MessageSenderValidationException) e);
+        } else if (e instanceof MessageSenderSoapFaultException) {
+            return new SoapFaultException((MessageSenderSoapFaultException) e);
         }
         return null;
     }
