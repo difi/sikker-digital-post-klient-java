9d0b3ff5070fd10d363bcb9bde318aedbb614391
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-12-27 12:18:50 +0100

    Sertifikatvalidering i SDP-klient
    - Kaster exception om ugyldig sertifikat
    - CertificateValidator: validerer ikke om miljø er null for å være bakoverkompatibel

--- src/main/java/no/difi/sdp/client2/internal/CertificateValidator.java
+++ src/main/java/no/difi/sdp/client2/internal/CertificateValidator.java
@@ -1,23 +1,35 @@
 package no.difi.sdp.client2.internal;
 
+import no.difi.sdp.client2.domain.Databehandler;
 import no.difi.sdp.client2.domain.Miljo;
+import no.difi.sdp.client2.domain.exceptions.SertifikatException;
 import no.digipost.security.cert.CertStatus;
 import no.digipost.security.cert.CertificateValidatorConfig;
 import no.digipost.security.cert.Trust;
-import org.apache.http.impl.client.CloseableHttpClient;
 
 import java.security.cert.X509Certificate;
+import java.text.MessageFormat;
 
 import static no.digipost.security.cert.OcspSetting.NO_OCSP;
 
 public class CertificateValidator {
 
-    public static CertStatus Validate(Miljo miljo, X509Certificate certificate) {
+    public static void Validate(Miljo miljo, X509Certificate certificate) {
+        if (miljo == null) {
+            return;
+        }
+
         Trust trusteChainCertificates = miljo.getGodkjenteKjedeSertifikater();
 
         CertificateValidatorConfig certificateValidatorConfig = CertificateValidatorConfig.MOST_STRICT.with(NO_OCSP);
         no.digipost.security.cert.CertificateValidator certificateValidator = new no.digipost.security.cert.CertificateValidator(certificateValidatorConfig, trusteChainCertificates, null);
 
-        return certificateValidator.validateCert(certificate);
+        CertStatus certStatus = certificateValidator.validateCert(certificate);
+
+        if (certStatus != CertStatus.OK) {
+            String message = MessageFormat
+                    .format("Sertifikatet som brukes for {0} er ikke gyldig. Prøver du å sende med et testsertifikat i produksjonsmiljø eller omvendt, eller er sertifikatet utgått?", Databehandler.class.getSimpleName());
+            throw new SertifikatException(message);
+        }
     }
 }
--- src/test/java/no/difi/sdp/client2/internal/CertificateValidatorTest.java
+++ src/test/java/no/difi/sdp/client2/internal/CertificateValidatorTest.java
@@ -1,9 +1,11 @@
 package no.difi.sdp.client2.internal;
 
 import no.difi.sdp.client2.domain.Miljo;
+import no.difi.sdp.client2.domain.exceptions.SertifikatException;
 import no.digipost.security.DigipostSecurity;
-import no.digipost.security.cert.CertStatus;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.security.cert.X509Certificate;
 
@@ -11,22 +13,36 @@ import static org.junit.Assert.assertEquals;
 
 public class CertificateValidatorTest {
 
+    public static final X509Certificate POSTEN_TEST_CERTIFICATE = DigipostSecurity.readCertificate("certificates/test/posten_test.pem");
+    public static final X509Certificate POSTEN_PROD_CERTIFICATE = DigipostSecurity.readCertificate("certificates/prod/posten_prod.pem");
+
     @Test
     public void accepts_test_certificate() {
-        X509Certificate certificate = DigipostSecurity.readCertificate("certificates/test/posten_test.pem");
-
-        CertStatus certStatus = CertificateValidator.Validate(Miljo.FUNKSJONELT_TESTMILJO, certificate);
-
-        assertEquals(CertStatus.OK, certStatus);
-
+        CertificateValidator.Validate(Miljo.FUNKSJONELT_TESTMILJO, POSTEN_TEST_CERTIFICATE);
     }
 
     @Test
     public void accepts_prod_certificate() {
-        X509Certificate certificate = DigipostSecurity.readCertificate("certificates/prod/posten_prod.pem");
+        CertificateValidator.Validate(Miljo.PRODUKSJON, POSTEN_PROD_CERTIFICATE);
+    }
 
-        CertStatus certStatus = CertificateValidator.Validate(Miljo.PRODUKSJON, certificate);
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
-        assertEquals(CertStatus.OK, certStatus);
+    @Test
+    public void stops_test_certificate_in_prod() {
+        thrown.expect(SertifikatException.class);
+        CertificateValidator.Validate(Miljo.PRODUKSJON, POSTEN_TEST_CERTIFICATE);
+    }
+
+    @Test
+    public void stops_prod_certificate_in_test() {
+        thrown.expect(SertifikatException.class);
+        CertificateValidator.Validate(Miljo.FUNKSJONELT_TESTMILJO, POSTEN_PROD_CERTIFICATE);
+    }
+
+    @Test
+    public void no_validation_if_environment_is_null(){
+        CertificateValidator.Validate(null, POSTEN_PROD_CERTIFICATE);
     }
 }
